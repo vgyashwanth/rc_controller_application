184ff7d2e93519987cc266a83383e27943d095ec
Yashwanth VG <vgyashwanthkumar@gmail.com>
2026-02-21 17:36:07 +0530

    receiver code development is done, future commits will be bugs

--- esp32code/receiver_code/receiver_code.ino
+++ esp32code/receiver_code/receiver_code.ino
@@ -1,12 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <Servo.h>
+#include <Ticker.h>
+
 
 // debug mode enable
- #define DEBUG_MODE 
+// #define DEBUG_MODE
 
-#define MOTOR_PIN 5       // D1-GPIO5
-#define SERVO_PIN 4       // D2-GPIO4
+// System related variables
+#define SERVO_PIN 5       // D1-GPIO5
+#define MOTOR_PIN  4      // D2-GPIO4
 #define SPOILER_PIN 0     // D3-GPIO0
 #define HORN_PIN 2        // D4-GPIO2
 #define HEAD_LIGHTS 12    // D6-GPIO12
@@ -14,8 +17,25 @@
 #define BOOSTER_LIGHTS 15 // D8-GPIO15
 #define RED_LIGHTS 1      // TX-GPIO1
 
-#define HORN_FREQUENCY 415 // frequency of the  Horn in Hz (McLaren 765LT)
-#define PARKING_LIGHT_FREQ 1500
+// frequency
+#define HORN_FREQUENCY 300 // frequency of the  Horn in Hz (McLaren 765LT)
+#define PARKING_LIGHT_FREQ 4500
+#define PARKING_LIGHT_BLINK_TIME 0.4 //sec
+#define BOOSTER_LIGHT_TOOGLE_TIME 0.05 // sec
+
+// Ticker to create the Tasks
+// Create two Ticker objects
+Ticker ticker1;
+Ticker ticker2;
+bool isParkingLightTickerActive = false;
+bool isBoosterLightTurnOnFirstTime = false;
+uint8_t gBoosterLightToggleCounter = 0;
+void ToggleBoosterLight(void);
+
+
+// Spolier related variables
+#define SPOILER_UP_ANGLE (90-24)
+#define SPOILER_DOWN_ANGLE (90+26)
 
 // Data packet
 struct DataPacket
@@ -37,6 +57,8 @@ void parseIncomingData(String sData, DataPacket *data);
 void SystemInit(void);
 void Control(DataPacket *rxData);
 void PrintReceivedData(DataPacket *data);
+void SpoilerOn(bool On);
+void parkingLights_ISR();
 
 Servo servo;        // create servo object to control a servo
 Servo motor;        // for controlling the esc of the motor
@@ -104,10 +126,6 @@ void loop()
       #endif
       // DECODE THE STRING HERE
       parseIncomingData(data, &gRxData);
-
-      #if defined(DEBUG_MODE)
-      PrintReceivedData(&gRxData);
-      #endif
 
       // Reply RSSI
       udp.beginPacket(udp.remoteIP(), udp.remotePort());
@@ -177,42 +195,179 @@ void Control(DataPacket *rxData)
   // Write the steering angle
   servo.write(int(rxData->steerAngle));
 
+  // process the head lights
+  if(rxData->headLights)
+  {
+     if(rxData->headLights == 1)
+     {
+        // Turn on the bar lights
+        digitalWrite(BAR_LIGHTS, HIGH);
+        // Turn off the other lights if already on
+        digitalWrite(HEAD_LIGHTS, LOW);
+        digitalWrite(RED_LIGHTS, LOW);
+     }
+     else
+     {
+        // Turn on all the lights
+        digitalWrite(BAR_LIGHTS, HIGH);
+        digitalWrite(HEAD_LIGHTS, HIGH);
+        digitalWrite(RED_LIGHTS, HIGH);
+     }
+  }
+  else 
+  {
+      // Turn off all the lights, only if the parking lights is off
+      if(rxData->parkingLights == false)
+      {
+        digitalWrite(BAR_LIGHTS, LOW);
+        digitalWrite(HEAD_LIGHTS, LOW);
+        digitalWrite(RED_LIGHTS, LOW);
+      }
+  }
+
+  // process the brake or if the car is going in reverse
+  if(rxData->brakeActive || ( rxData->throttlePWM < 1500 ))
+  { 
+    if(rxData->parkingLights == false)
+    {
+      digitalWrite(RED_LIGHTS, HIGH);
+    }
+  }
+  else
+  {
+    // Turn off only if their are not turned on using head lights and not turned on using parking lights also
+    if((rxData->headLights == 1) && (rxData->parkingLights == false))
+    {
+      digitalWrite(RED_LIGHTS, LOW);
+    }
+
+  }
+
+  // process the spoiler
+  if(rxData->spoilerState)
+  {
+      SpoilerOn(HIGH);
+  }
+  else 
+  {
+      SpoilerOn(LOW);
+  }
+
+
   // process the horn
   if(rxData->hornActive)
   {
     tone(HORN_PIN, HORN_FREQUENCY, 100);
   }
-
-  if(rxData->parkingLights)
-  {
-    tone(HORN_PIN, PARKING_LIGHT_FREQ, 100);
-    delay(500);
-    tone(HORN_PIN, PARKING_LIGHT_FREQ, 100);
-    delay(500);
-
+  else 
+  { 
+    if(rxData->parkingLights == false)
+    {
+      noTone(HORN_PIN);
+    }
   }
 
+  // process the parking lights
+  if(rxData->parkingLights)
+  {
+    if(isParkingLightTickerActive == false)
+    {
+      ticker1.attach(PARKING_LIGHT_BLINK_TIME, parkingLights_ISR);
+      isParkingLightTickerActive = true;
+    }
+
+  }
+  else
+  {
+      ticker1.detach();
+      isParkingLightTickerActive = false;
+  }
+
+  // process the booster light
+  // it should not blink the light during brake
+  if((rxData->throttlePWM > 1800) && (rxData->brakeActive == false) )
+  { 
+    #define TOOGLE_COUNTER_MAX 25
+    if(isBoosterLightTurnOnFirstTime == false)
+    {
+      ticker2.attach(BOOSTER_LIGHT_TOOGLE_TIME, ToggleBoosterLight);
+      isBoosterLightTurnOnFirstTime = true;
+    }
+    else if(gBoosterLightToggleCounter > TOOGLE_COUNTER_MAX)
+    {
+      ticker2.detach();
+      digitalWrite(BOOSTER_LIGHTS, HIGH);
+    }
+    else
+    {
+      // Nothing to do
+    }
+
+  }
+  else
+  { 
+      ticker2.detach();
+      digitalWrite(BOOSTER_LIGHTS, LOW);
+      isBoosterLightTurnOnFirstTime = false;
+      gBoosterLightToggleCounter = 0;  
+  }
+
+
+}
+
+void SpoilerOn(bool On)
+{   
+  // Read the Current Spoiler Postion
+  uint8_t current_angle = spoiler.read();
+
+  if(On)
+  {
+
+    for(uint8_t angle = current_angle; angle>=SPOILER_UP_ANGLE; angle-=2)
+    {
+        spoiler.write(angle);
+    }
+
+  }
+  else
+  {
+    for(uint8_t angle = current_angle; angle<=SPOILER_DOWN_ANGLE; angle+=2)
+    {
+        spoiler.write(angle);
+    }
+  }
+  
 }
 
 
-void PrintReceivedData(DataPacket *data)
+
+void parkingLights_ISR()
 {
-  // Serial printing for debugging the extracted values
-  Serial.println("--- Incoming Control Data ---");
+    // blink the parking lights with tone,
+    static bool state = 0;
+    state = state^1; // do the xor to flip the state
+    digitalWrite(BAR_LIGHTS, state);
+    digitalWrite(RED_LIGHTS, state);
 
-  Serial.print("Steer: "); Serial.print(data->steerAngle);
-  Serial.print(" | PWM: "); Serial.print(data->throttlePWM);
-  Serial.print(" | REV: "); Serial.println(data->reverseActive ? "YES" : "NO");
+    if(state)
+    {
+      tone(HORN_PIN, PARKING_LIGHT_FREQ, 1000);
+    }
+    else
+    {
+      noTone(HORN_PIN);
+    }
 
-  Serial.print("Brake: "); Serial.print(data->brakeActive ? "ON" : "OFF");
-  Serial.print(" | Headlights: "); Serial.print(data->headLights);
-  Serial.print(" | Parking: "); Serial.println(data->parkingLights ? "ON" : "OFF");
-
-  Serial.print("Spoiler: "); Serial.print(data->spoilerState ? "UP" : "DOWN");
-  Serial.print(" | Horn: "); Serial.print(data->hornActive ? "ACTIVE" : "OFF");
-  Serial.print(" | S2: "); Serial.print(data->s2Active);
-  Serial.print(" | S3: "); Serial.println(data->s3Active);
-
-  Serial.println("-----------------------------");
 
 }
+
+void ToggleBoosterLight(void)
+{
+  static bool state = 0;
+  state ^=1;
+  digitalWrite(BOOSTER_LIGHTS,state);
+  gBoosterLightToggleCounter++;
+
+}
+
+
